8c8c6e0e656c80bd40e81e2118505ea4aee27370
Thomas Tsuma <34827918+TomiTsuma@users.noreply.github.com>
2023-04-19 21:46:46 -0700

    Update dvc_setup.sh

--- dvc_setup.sh
+++ dvc_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Run these commands in a bash terminal (Supply the Access credentials and github username first)
-# pip install dvc --user
-# pip install "dvc[s3]" --user
+pip install dvc --user
+pip install "dvc[s3]" --user
 DO_AWS_ACCESS_KEY_ID=DO00JVCF7VU9NR8R9KUC
 DO_AWS_ACCESS_KEY=qw7fwzXbo90pYNVgybQbodide616IklbcR21y6G8EOI   
 
 # cd D://CropNutsDocuments/MSSC_DVC
-# python -m dvc init
+python -m dvc init -f
 python -m dvc remote add --default storage s3://spectral-models -f
 python -m dvc remote modify storage endpointurl https://ams3.digitaloceanspaces.com
 
@@ -14,8 +14,8 @@ python -m dvc remote modify storage endpointurl https://ams3.digitaloceanspaces.
 export AWS_ACCESS_KEY_ID=${DO_AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${DO_AWS_ACCESS_KEY}
 
-python -m dvc add data.zip
-python -m dvc push data.zip
+python -m dvc add high_low_val_method_1.zip
+python -m dvc push high_low_val_method_1.zip
 
 
 
