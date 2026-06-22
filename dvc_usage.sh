a3437ad6d445f1954cd490cde2013072a5728486
TomiTsuma <tommytsuma7@gmail.com>
2026-06-22 05:52:06 +0000

    Latest update

--- dvc_usage.sh
+++ dvc_usage.sh
@@ -1,7 +1,3 @@
-DO_AWS_ACCESS_KEY_ID=DO00JVCF7VU9NR8R9KUC
-DO_AWS_ACCESS_KEY=qw7fwzXbo90pYNVgybQbodide616IklbcR21y6G8EOI 
-
-
 # Clone the repositroy containing dvc files
 # git clone https://github.com/${DO_GITHUB_USERNAME}/MSSC_DVC_V2.git MSSC_DVC
 
